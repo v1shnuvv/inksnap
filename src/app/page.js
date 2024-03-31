0a6349fc322dcabb53648791c32935fa30ccde9e
VishnuV <vishnuv4363@gmail.com>
2024-04-01 02:37:33 +0530

    nextjs setup

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <main >
+      <div >
+        <p>
+          Get started by editing&nbsp;
+        </p>   
+      </div>
+
+    </main>
+  )
+}

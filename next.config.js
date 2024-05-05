49099a49e59b82e4a8200dff7cb0e5cb603c9e81
VishnuV <vishnuv4363@gmail.com>
2024-05-05 20:01:33 +0530

    fix typo error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === "production"
-
 const nextConfig = {
     output: "export",
     distDir: 'dist',
     basePath: isProd ? '/inksnap' : '',
-    "paths": {
-        "@/components/*": ["src/components/*"],
-        "@/context/*": ["src/context/*"],
-        "@/provider/*": ["src/provider/*"]
-    }
+    images: { unoptimized: true }
 }
 
 module.exports = nextConfig
